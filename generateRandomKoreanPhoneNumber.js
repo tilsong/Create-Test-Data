22c26dde6dc756f8187d98c8967017516a9571be
tilsong <cnsong0229@gmail.com>
2023-05-22 13:51:19 +0900

    Feat: 한국 이름 생성 함수와 한국 휴대전화 번호 생성 함수 작성

---
+++ generateRandomKoreanPhoneNumber.js
@@ -0,0 +1,15 @@
+function generateRandomKoreanPhoneNumber() {
+    let phoneNumber = "010";
+  
+    phoneNumber += Math.floor(Math.random() * 9) + 1;
+
+    for (let i = 0; i < 7; i++) {
+        phoneNumber += Math.floor(Math.random() * 10);
+    }
+  
+    return phoneNumber;
+}
+  
+module.exports = {
+    generateRandomKoreanPhoneNumber: generateRandomKoreanPhoneNumber
+};
