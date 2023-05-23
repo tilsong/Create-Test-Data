01a02794913ab3da02768538dff6166aa8aac2b5
tilsong <cnsong0229@gmail.com>
2023-05-23 23:01:27 +0900

    Fix: gererateRandomUserSql 기능을 모듈로 빼고, 오타 수정

--- crawling/crawlingWithSpecialty.js
+++ crawling/crawlingWithSpecialty.js
@@ -4,29 +4,29 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 
 const crawling_list = [
-  ["물류", "https://soomgo.com/search/pro/포장-물류-대행/address/비즈니스-아웃소싱--null/review_count"],
-  ["전문직", "https://soomgo.com/search/pro/service/address/비즈니스--회계-세무-재무/review_count"],
-  ["여행", "https://soomgo.com/search/pro/service/address/기타--여행/review_count"],
-  ["육아", "https://soomgo.com/search/pro/산후-육아-아이돌보미/address/홈-리빙--도우미/review_count"],
-  ["금융", "https://soomgo.com/search/pro/service/address/기타--금융/review_count"], 
-  ["건설", "https://soomgo.com/search/pro/공사건설현장-알바/address알바--생산기능노무-알바/review_count"],
-  ["서비스", "https://soomgo.com/search/pro/service/address/알바/review_count"],
-  ["경영지원", "https://soomgo.com/search/pro/경영-컨설팅/address/비즈니스-컨설팅/review_count"],
-  ["의료", "https://soomgo.com/search/pro/service/address/건강-미용--심리/review_count"],
-  ["법무", "https://soomgo.com/search/pro/service/address/기타--법률/review_count"],
-  ["마케팅", "https://soomgo.com/search/pro/service/address/비즈니스--마케팅/review_count"],
-  ["프로그래밍", "https://soomgo.com/search/pro/service/address/디자인-개발--개발-외주/review_count"],
-  ["인적자원", "https://soomgo.com/search/pro/service/address/비즈니스--인사/review_count"],
-  ["교육", "https://soomgo.com/search/pro/service/address/레슨--학업/review_count"],
-  ["헬스", "https://soomgo.com/search/pro/퍼스널트레이닝/address/건강-미용--건강/review_count"],
-  ["무역", "https://soomgo.com/search/pro/무역대행-수입수출포워딩등/address/비즈니스--아웃소싱/review_count"],
-  ["생활", "https://soomgo.com/search/pro/service/address/홈-리빙--수도-보일러-전기/review_count"],
-  ["예술", "https://soomgo.com/search/pro/service/address/레슨--미술/review_count"],
-  ["기타", "https://soomgo.com/search/pro/service/address/레슨--취업준비/review_count"],
-  ["디자인", "https://soomgo.com/search/pro/service/address/디자인-개발--디자인-외주/review_count"]
+  ["CONSTRUCTION", "https://soomgo.com/search/pro/공사건설현장-알바/address알바--생산기능노무-알바/review_count"],
+  ["LOGISTICS", "https://soomgo.com/search/pro/포장-물류-대행/address/비즈니스-아웃소싱--null/review_count"],
+  ["PROFESSIONAL", "https://soomgo.com/search/pro/service/address/비즈니스--회계-세무-재무/review_count"],
+  ["TRAVEL", "https://soomgo.com/search/pro/service/address/기타--여행/review_count"],
+  ["PARENTING", "https://soomgo.com/search/pro/산후-육아-아이돌보미/address/홈-리빙--도우미/review_count"],
+  ["FINANCE", "https://soomgo.com/search/pro/service/address/기타--금융/review_count"], 
+  ["SERVICE", "https://soomgo.com/search/pro/service/address/알바/review_count"],
+  ["BS", "https://soomgo.com/search/pro/경영-컨설팅/address/비즈니스-컨설팅/review_count"],
+  ["HEALTHCARE", "https://soomgo.com/search/pro/service/address/건강-미용--심리/review_count"],
+  ["LEGAL", "https://soomgo.com/search/pro/service/address/기타--법률/review_count"],
+  ["MARKETING", "https://soomgo.com/search/pro/service/address/비즈니스--마케팅/review_count"],
+  ["PROGRAMMING", "https://soomgo.com/search/pro/service/address/디자인-개발--개발-외주/review_count"],
+  ["HR", "https://soomgo.com/search/pro/service/address/비즈니스--인사/review_count"],
+  ["EDUCATION", "https://soomgo.com/search/pro/service/address/레슨--학업/review_count"],
+  ["HEALTH", "https://soomgo.com/search/pro/퍼스널트레이닝/address/건강-미용--건강/review_count"],
+  ["TRADE", "https://soomgo.com/search/pro/무역대행-수입수출포워딩등/address/비즈니스--아웃소싱/review_count"],
+  ["LIFESTYLE", "https://soomgo.com/search/pro/service/address/홈-리빙--수도-보일러-전기/review_count"],
+  ["ART", "https://soomgo.com/search/pro/service/address/레슨--미술/review_count"],
+  ["OTHER", "https://soomgo.com/search/pro/service/address/레슨--취업준비/review_count"],
+  ["DESIGN", "https://soomgo.com/search/pro/service/address/디자인-개발--디자인-외주/review_count"]
 ];
 
-const separation = "\t ... \n";
+const separation = "&||";
 
 const scrollToEnd = async (page) => {
   let previousHeight = 0;
